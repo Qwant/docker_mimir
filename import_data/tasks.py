b74cfe54a06cbc83944c1adb978ee51382caf270
Antoine D <antoine.desbordes@gmail.com>
2018-06-11 11:10:50 +0200

    rewrite configuration organization

--- import_data/tasks.py
+++ import_data/tasks.py
@@ -1,7 +1,9 @@
 
 from invoke import task
+from invoke.config import DataProxy
 import logging
 
+logging.basicConfig(level=logging.INFO)
 
 def run_rust_binary(ctx, bin, params):
     logging.debug('running: {bin} {params}'.format(**locals()))
@@ -10,25 +12,28 @@ def run_rust_binary(ctx, bin, params):
 
 @task()
 def generate_cosmogony(ctx):
-    with ctx.cd(ctx.cosmogony.directory):
-        ctx.run('mkdir -p {ctx.cosmogony.output_dir}'.format(**locals()))
-        cosmogony_file = '{ctx.cosmogony.output_dir}/cosmogony.json'.format(**locals())
+    logging.info("generating cosmogony file")
+    with ctx.cd(ctx.admin.cosmogony.directory):
+        ctx.run('mkdir -p {ctx.admin.cosmogony.output_dir}'.format(ctx=ctx))
+        cosmogony_file = '{ctx.admin.cosmogony.output_dir}/cosmogony.json'.format(ctx=ctx)
         run_rust_binary(ctx, 'cosmogony', 
         '--input {ctx.osm_file} \
-        --output {cosmogony_file}'.format(**locals()))
-        ctx.cosmogony.file = cosmogony_file
+        --output {cosmogony_file}'.format(ctx=ctx, cosmogony_file=cosmogony_file))
+        ctx.admin.cosmogony.file = cosmogony_file
 
 
 @task()
 def load_cosmogony(ctx):
+    logging.info("loading cosmogony")
     run_rust_binary(ctx, 'cosmogony2mimir', 
-        '--input {ctx.cosmogony.file} \
+        '--input {ctx.admin.cosmogony.file} \
         --connection-string {ctx.es} \
-        --dataset {ctx.dataset}'.format(**locals()))
+        --dataset {ctx.dataset}'.format(ctx=ctx))
 
 
 @task()
 def load_osm(ctx):
+    logging.info("importing data from osm")
     import_admin = '' if _use_cosmogony(ctx) else '--import-admin'
 
     run_rust_binary(ctx, 'osm2mimir', 
@@ -36,49 +41,66 @@ def load_osm(ctx):
         --connection-string {ctx.es} \
         --dataset {ctx.dataset}\
         --import-way \
-        {import_admin}'.format(**locals()))
+        {import_admin}'.format(ctx=ctx, import_admin=import_admin))
 
 
 @task()
 def load_addresses(ctx):
-    if 'bano_file' in ctx:
+    addr_config = ctx.get('addresses')
+    if not _is_config_object(addr_config):
+        logging.info("no addresses to import")
+        return
+
+    if 'bano_file' in addr_config:
+        logging.info("importing bano addresses")
         run_rust_binary(ctx, 'bano2mimir', 
-            '--input {ctx.bano_file} \
+            '--input {ctx.addresses.bano_file} \
             --connection-string {ctx.es} \
-            --dataset {ctx.dataset}'.format(**locals()))
-    if 'oa_file' in ctx:
+            --dataset {ctx.dataset}'.format(ctx=ctx))
+    if 'oa_file' in addr_config:
+        logging.info("importing oa addresses")
         # TODO take multiples oa files ?
         run_rust_binary(ctx, 'openaddresses2mimir', 
-            '--input {ctx.oa_file} \
+            '--input {ctx.addresses.oa_file} \
             --connection-string {ctx.es} \
-            --dataset {ctx.dataset}'.format(**locals()))
+            --dataset {ctx.dataset}'.format(ctx=ctx))
 
 
 @task()
 def load_pois(ctx):
-    if 'poi' not in ctx:
+    poi_conf = ctx.get('poi')
+    if not _is_config_object(poi_conf):
         logging.info("no poi to import")
         return
 
-    if 'fafnir' in ctx.poi:
-        if ctx.poi.fafnir.get('load_db') is True:
+    fafnir_conf = poi_conf.get('fafnir')
+    if _is_config_object(fafnir_conf):
+        if fafnir_conf.get('load_db') is True:
             # TODO import data in PG
             logging.warn("for the moment we can't load data in postgres for fafnir")
 
+        logging.info("importing poi with fafnir")
         run_rust_binary(ctx, 'fafnir', 
             '--es {ctx.es} \
-            --pg {ctx.poi.fafnir.pg}'.format(**locals()))
-    else:
+            --pg {fafnir_conf.pg}'.format(ctx=ctx, fafnir_conf=fafnir_conf))
+            
+    if 'osm' in poi_conf:
+        logging.info("importing poi from osm")
         # TODO take a custom poi_config
         run_rust_binary(ctx, 'osm2mimir', 
             '--input {ctx.osm_file} \
             --connection-string {ctx.es} \
             --dataset {ctx.dataset}\
-            --import-poi'.format(**locals()))
+            --import-poi'.format(ctx=ctx))
 
 
 def _use_cosmogony(ctx):
-    return 'cosmogony' in ctx
+    admin_conf = ctx.get('admin')
+    return _is_config_object(admin_conf) and 'cosmogony' in admin_conf
+
+
+def _is_config_object(obj):
+    return obj is not None and (isinstance(obj, DataProxy) or isinstance(obj, dict))
 
 
 @task(default=True)
@@ -88,7 +110,8 @@ def load_all(ctx):
     This is the main tasks that import all the datas into mimir
     """
     if _use_cosmogony(ctx):
-        if 'file' not in ctx.cosmogony:
+        logging.info('using cosmogony')
+        if not ctx.admin.cosmogony.get('file'):
             generate_cosmogony(ctx)
         load_cosmogony(ctx)
 
