a5dcd4972310502e8fd48b348cdb81ed9120b941
Rémi Dupré <r.dupre@qwant.com>
2020-02-25 17:28:15 +0100

    re-download files only periodically

--- download/tasks.py
+++ download/tasks.py
@@ -1,38 +1,132 @@
 import fnmatch
+import hashlib
+import json
 import re
+import sys
 import tempfile
+import requests
+from datetime import datetime, timedelta
 from os import path, walk
 
 from invoke import task
 
 
+STATUS_FILE_NAME = "_files_status.json"
+
+
+def raw_files_status(ctx):
+    files_status_path = path.join(ctx.cache_dir, STATUS_FILE_NAME)
+
+    if path.isfile(files_status_path):
+        with open(files_status_path) as data:
+            return json.load(data)
+    else:
+        return dict()
+
+
+def get_file_status(ctx, filename):
+    res = raw_files_status(ctx).get(filename)
+
+    if res is None:
+        res = {"last_update": None, "md5": None}
+
+    if res["last_update"] is not None:
+        res["last_update"] = datetime.utcfromtimestamp(res["last_update"])
+
+    return res
+
+
+def save_file_status(ctx, filename, status):
+    files_status_path = path.join(ctx.cache_dir, STATUS_FILE_NAME)
+
+    if status is not None:
+        status["last_update"] = status["last_update"].timestamp()
+
+    files_status = raw_files_status(ctx)
+    files_status[filename] = status
+
+    with open(files_status_path, "w") as data:
+        json.dump(files_status, data)
+
+
+def needs_to_download(ctx, filename, max_age=None, md5_url=None):
+    """
+    Check if "filename" should be downloaded because it doesn't exist or is
+    older than "max_age" or its checksum has changed.
+    """
+    if not path.isfile(filename):
+        return True
+
+    file_status = get_file_status(ctx, filename)
+
+    if max_age is not None and (
+        file_status["last_update"] is None
+        or datetime.utcnow() - file_status["last_update"] > max_age
+    ):
+        return True
+
+    if md5_url is not None:
+        res = requests.get(md5_url)
+        expt_md5 = res.text.split()[0]
+        curr_md5 = file_status["md5"]
+
+        if expt_md5 != curr_md5:
+            return True
+
+    if ctx.force_downloads:
+        print(
+            f"'force_downloads' is set to true: existing filename {filename} will be ignored",
+            file=sys.stderr,
+        )
+        return True
+
+    print(
+        f"filename '{filename}' already exists, we don't need to download it again",
+        file=sys.stderr,
+    )
+    return False
+
+
+def download_file(ctx, filename, url, max_age=None, md5_url=None):
+    if not needs_to_download(ctx, filename, max_age, md5_url):
+        return
+
+    #  Forget current informations about file in case the download fails.
+    save_file_status(ctx, filename, None)
+
+    ctx.run(f"mkdir -p {path.dirname(filename)}/")
+    ctx.run(f"wget --progress=dot:giga -O {filename} {url}")
+
+    with open(filename, "rb") as f:
+        md5 = hashlib.md5()
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5.update(chunk)
+        md5 = md5.hexdigest()
+
+    save_file_status(ctx, filename, {"last_update": datetime.utcnow(), "md5": md5})
+
+
 @task
 def download_osm(ctx, osm_url, output_file):
-    ctx.run(f"mkdir -p {path.dirname(output_file)}")
-    ctx.run(f"wget --progress=dot:giga {osm_url} -O {output_file}")
+    download_file(ctx, output_file, osm_url, md5_url=osm_url + ".md5")
 
 
 @task
 def download_bano(ctx, bano_url, output_file):
-    ctx.run(f"mkdir -p {path.dirname(output_file)}")
-    gzip_file = f"{output_file}.gz"
-    ctx.run(f"wget --progress=dot:giga {bano_url} -O {gzip_file}")
-    ctx.run(f"gunzip -f {gzip_file}")
+    src_file = path.join(ctx.cache_dir, "bano.csv.gz")
+    download_file(ctx, src_file, bano_url, max_age=timedelta(days=7))
+    ctx.run(f"gunzip -c {src_file} > {output_file}")
 
 
 @task
-def download_oa(ctx, oa_filter):
-    tmp_dir = tempfile.mkdtemp()
+def download_oa(ctx, oa_filter, output_dir):
+    src_file = path.join(ctx.cache_dir, "oa.zip")
+    download_file(ctx, src_file, ctx.oa_url, max_age=timedelta(days=7))
 
-    tmp_file = path.join(tmp_dir, "oa.zip")
-    ctx.run(f"mkdir -p {path.dirname(tmp_file)}")
-    ctx.run(f"wget --progress=dot:giga {ctx.oa_url} -O {tmp_file}")
+    oa_tmp_dir = path.join(ctx.tmp_dir, "oa")
+    ctx.run(f"unzip -o -qq -d {oa_tmp_dir} {src_file}")
 
-    oa_tmp_dir = path.join(tmp_dir, "oa")
-    ctx.run(f"unzip -o -qq -d {oa_tmp_dir} {tmp_file}")
-    ctx.run(f"rm {tmp_file}")
-
-    # Collect the list of files to include in the output
+    # Collect the list of files to include in the output.
     keep_patterns = (fnmatch.translate(pat) for pat in oa_filter.split(","))
     pattern = re.compile("|".join(keep_patterns))
     included_files = []
@@ -45,12 +139,13 @@ def download_oa(ctx, oa_filter):
             if pattern.match(relt_path):
                 included_files.append(full_path)
 
-    # Flatten all .csv to the root of a single director
-    output_dir = path.join(ctx.data_dir, "addresses", "oa")
+    # Flatten all .csv into output directory.
+    print("Collect OpenAddresses data")
+
     ctx.run(f"mkdir -p {output_dir}")
     ctx.run(f"rm -rf {output_dir}/*")
 
     for filename in included_files:
         flat_name = path.relpath(filename, oa_tmp_dir).replace("/", "__")
-        print(f"{flat_name:>40} ...", flush=True)
+        print(f" -> add {flat_name}", flush=True)
         ctx.run(f"mv {filename} {output_dir}/{flat_name}")
